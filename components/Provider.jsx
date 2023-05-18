438fca184963c9948f3f3b031c47adcbc7319e01
Gumeq <64758768+Gumeq@users.noreply.github.com>
2023-05-18 06:07:18 +0300

    new

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,10 +1,11 @@
-"use client";
-
-import React from "react";
+'use client';
 
 import { SessionProvider } from "next-auth/react";
 
 const Provider = ({ children, session }) => (
-	<SessionProvider session={session}>{children}</SessionProvider>
-);
+  <SessionProvider session={session}>
+    {children}
+  </SessionProvider>
+)
+
 export default Provider;
